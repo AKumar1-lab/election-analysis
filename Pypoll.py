89005939336568278782ed7dac1dcdca8d187df0
AKumar1-lab <85860367+AKumar1-lab@users.noreply.github.com>
2021-07-12 00:03:42 -0700

    election results

--- Pypoll.py
+++ Pypoll.py
@@ -75,7 +75,7 @@ for candidate_name in candidate_votes:
     print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
     # Determine winning vote count and candidate
     # Determine if the votes is greater than the winning count.
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
     if (votes > winning_count) and (vote_percentage > winning_percentage):
          # If true then set winning_count = votes and winning_percent =
          # vote_percentage.
@@ -92,11 +92,19 @@ winning_candidate_summary = (
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
-print(winning_candidate_summary)
+#print(winning_candidate_summary)
 
-
+# Add a vote to that candidate's count
+candidate_votes[candidate_name] += 1
+# Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
-   txt_file.write("Counties in the Election\n")
-   txt_file.write("-" * 20)
-   txt_file.write("\nArapahoe\nDenver\nJefferson")
+# Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
 txt_file.close()
